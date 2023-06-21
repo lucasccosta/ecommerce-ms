729f14b484ecacf993d191bc27b83462a1a711fb
Lucas <lucas_ccg@hotmail.com>
2023-06-20 23:34:36 -0300

    changing direcotry

--- customers/src/shared/container/index.ts
+++ customers/src/shared/container/index.ts
@@ -1,6 +1,6 @@
 import { container } from "tsyringe";
-import { ICustomersRepository } from "../../infra/repositories/ICustomersRepository";
-import { CustomersRepository } from "../../infra/repositories/CustomersRepository";
+import { ICustomersRepository } from "../../infra/repositories/customer/ICustomersRepository";
+import { CustomersRepository } from "../../infra/repositories/customer/CustomersRepository";
 
 container.registerSingleton<ICustomersRepository>(
   "CustomersRepository",
