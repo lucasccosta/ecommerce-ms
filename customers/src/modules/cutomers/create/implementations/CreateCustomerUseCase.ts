c99be791389e3e749b5cbfa103c9336d3d3ac60e
Lucas <lucas_ccg@hotmail.com>
2023-06-20 23:38:13 -0300

    changing logic to add event dispatch

--- customers/src/modules/cutomers/create/implementations/CreateCustomerUseCase.ts
+++ customers/src/modules/cutomers/create/implementations/CreateCustomerUseCase.ts
@@ -1,20 +1,31 @@
-import { ICustomersRepository } from "../../../../infra/repositories/ICustomersRepository";
-import { IKafkaProducer } from "../../../../infra/provider/kafka/IKafkaProducer";
+import { ICustomersRepository } from "../../../../infra/repositories/customer/ICustomersRepository";
 import { Customer } from "../../../domain/Customer";
 import { v4 as uuid } from "uuid";
 import { CustomError } from "../CustomError";
 import { ICreateCustomerUseCase } from "../ICreateCustomerUsecase";
 import { ICreateCustomerRequest } from "../../DTOs";
 import { HttpResponse } from "../../../../infra/DTOs";
+import IEventDispatcher from "../../../../shared/event/IEventDispatcher";
+import CustomerCreatedEvent from "../../event/CustomerCreatedEvent";
+import IEventHandler from "../../../../shared/event/IEventHandler";
 
 export class CreateCustomerUseCase implements ICreateCustomerUseCase {
 
   private customersRepository: ICustomersRepository;
-  private kafkaProducer: IKafkaProducer
+  private eventDispatcher: IEventDispatcher
+  private customerCreatedHandler: IEventHandler<CustomerCreatedEvent>
+  private saveAtEventStoreHandler: IEventHandler<CustomerCreatedEvent>
 
-  constructor(customersRepository: ICustomersRepository, kafkaProducer: IKafkaProducer) {
+  constructor(
+    customersRepository: ICustomersRepository,
+    eventDispatcher: IEventDispatcher,
+    customerCreatedHandler: IEventHandler<CustomerCreatedEvent>,
+    saveAtEventStoreHandler: IEventHandler<CustomerCreatedEvent>
+  ) {
     this.customersRepository = customersRepository;
-    this.kafkaProducer = kafkaProducer;
+    this.eventDispatcher = eventDispatcher;
+    this.customerCreatedHandler = customerCreatedHandler;
+    this.saveAtEventStoreHandler = saveAtEventStoreHandler;
   }
 
   async execute({
@@ -26,7 +37,7 @@ export class CreateCustomerUseCase implements ICreateCustomerUseCase {
     let customer = await this.customersRepository.getCustomerByEmail(email);
     if (customer) throw new CustomError(`Customer already exists`, 409);
     customer = new Customer({id: uuid(), email, name, password, phone})
-
+    
     try {
       const customerCreated = await this.customersRepository.createCustomer({
         id: customer.id,
@@ -35,8 +46,12 @@ export class CreateCustomerUseCase implements ICreateCustomerUseCase {
         password: customer.password,
         phone: customer.phone,
       });
-
-      // await this.kafkaProducer.sendMessage('CUSTOMER_CREATED', customerCreated)
+      
+      
+      this.eventDispatcher.register("CustomerCreatedEvent", this.customerCreatedHandler)
+      this.eventDispatcher.register("CustomerCreatedEvent", this.saveAtEventStoreHandler)
+      const customerCreatedEvent = new CustomerCreatedEvent({id: customer.id,email: customer.email})
+      this.eventDispatcher.notify(customerCreatedEvent)
       return {
         body: customerCreated,
         statusCode: 201
