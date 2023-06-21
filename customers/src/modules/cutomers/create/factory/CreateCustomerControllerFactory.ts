aac594d7d50be12a80f8f8b40c4d64a974770ac9
Lucas <lucas_ccg@hotmail.com>
2023-06-20 23:34:55 -0300

    add instances

--- customers/src/modules/cutomers/create/factory/CreateCustomerControllerFactory.ts
+++ customers/src/modules/cutomers/create/factory/CreateCustomerControllerFactory.ts
@@ -1,5 +1,9 @@
 import { KafkaProducer } from "../../../../infra/provider/kafka/KafkaProducer"
-import { CustomersRepository } from "../../../../infra/repositories/CustomersRepository"
+import { CustomersRepository } from "../../../../infra/repositories/customer/CustomersRepository"
+import { EventsRepository } from "../../../../infra/repositories/eventsRepository/EventsRepository"
+import EventDispatcher from "../../../../shared/event/EventDispatcher"
+import CustomerCreatedHandler from "../../event/handlers/CustomerCreatedHandler"
+import SaveAtEventStoreHandler from "../../event/handlers/SaveAtEventStoreHandler"
 import { CreateCustomerController } from "../implementations/CreateCustomerController"
 import { CreateCustomerUseCase } from "../implementations/CreateCustomerUseCase"
 
@@ -7,7 +11,16 @@ import { CreateCustomerUseCase } from "../implementations/CreateCustomerUseCase"
 export const createCustomerControllerFactory = () => {
   const customersRepository = new CustomersRepository()
   const kafkaProducer = new KafkaProducer()
-  const createCustomerUseCase = new CreateCustomerUseCase(customersRepository, kafkaProducer)
+  const eventDispatcher = new EventDispatcher()
+  const customerCreatedHandler = new CustomerCreatedHandler(kafkaProducer)
+  const eventsRepository = new EventsRepository()
+  const saveAtEventStoreHandler = new SaveAtEventStoreHandler(eventsRepository)
+  const createCustomerUseCase = new CreateCustomerUseCase(
+    customersRepository,
+    eventDispatcher,
+    customerCreatedHandler,
+    saveAtEventStoreHandler
+    )
   const createCustomerController = new CreateCustomerController(createCustomerUseCase)
   return createCustomerController
 }
